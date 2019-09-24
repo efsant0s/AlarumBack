a927ce1bc180bb8fcd097b259a4c78c155543a45
efsant0s <eduardo.felipe.santos@philips.com>
2019-09-24 19:14:17 -0300

    Commit inicial

---
+++ src/main/java/com/senior/alarumback/MainApp.java
@@ -0,0 +1,145 @@
+package com.senior.alarumback;
+
+import java.awt.TrayIcon;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.awt.AWTException;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javax.imageio.ImageIO;
+
+public class MainApp extends Application {
+
+    private boolean firstTime;
+    private TrayIcon trayIcon;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        createTrayIcon(stage);
+        firstTime = true;
+        Platform.setImplicitExit(false);
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void createTrayIcon(final Stage stage) {
+        if (SystemTray.isSupported()) {
+            // get the SystemTray instance
+            SystemTray tray = SystemTray.getSystemTray();
+            // load an image
+            java.awt.Image image = null;
+            try {
+                URL url = new URL("http://www.digitalphotoartistry.com/rose1.jpg");
+                image = ImageIO.read(url);
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent t) {
+                    hide(stage);
+                }
+            });
+            // create a action listener to listen for default action executed on the tray icon
+            final ActionListener closeListener = new ActionListener() {
+                @Override
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.exit(0);
+                }
+            };
+
+            ActionListener showListener = new ActionListener() {
+                @Override
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            stage.show();
+                        }
+                    });
+                }
+            };
+            // create a popup menu
+            PopupMenu popup = new PopupMenu();
+
+            MenuItem showItem = new MenuItem("Mostrar");
+            showItem.addActionListener(showListener);
+            popup.add(showItem);
+
+            MenuItem closeItem = new MenuItem("Fechar");
+            closeItem.addActionListener(closeListener);
+            popup.add(closeItem);
+            /// ... add other items
+            // construct a TrayIcon
+            trayIcon = new TrayIcon(image, "Title", popup);
+            // set the TrayIcon properties
+            trayIcon.addActionListener(showListener);
+            // ...
+            // add the tray image
+            try {
+                tray.add(trayIcon);
+            } catch (AWTException e) {
+                System.err.println(e);
+            }
+            // ...
+        }
+    }
+
+    public void showProgramIsMinimizedMsg() {
+        if (firstTime) {
+            trayIcon.displayMessage("Ainda rodando.",
+                    "A aplicação vai continuar de fundo.",
+                    TrayIcon.MessageType.INFO);
+            firstTime = false;
+        }
+    }
+
+    private void hide(final Stage stage) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                if (SystemTray.isSupported()) {
+                    stage.hide();
+                    showProgramIsMinimizedMsg();
+                } else {
+                    System.exit(0);
+                }
+            }
+        });
+    }
+}
